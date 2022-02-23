a7e3608b433b5120e086e9072d0ed0f399e7820f
Juan Carlos Garcia <jcgseco@gmail.com>
2022-02-23 19:22:10 +0100

    Add commons project to support event tracking and crash logging

---
+++ feat-base/src/main/java/com/jcgseco/myarmory/core/commons/logger/CoordinatorLogger.kt
@@ -0,0 +1,26 @@
+package com.jcgseco.myarmory.core.commons.logger
+
+import com.jcgseco.myarmory.core.commons.data.preferences.SessionPreferences
+import javax.inject.Inject
+
+class CoordinatorLogger @Inject constructor(
+    sessionPreferences: SessionPreferences
+) : Logger {
+
+    private val loggers = listOf(
+        AndroidLogger(),
+        CrashlyticsLogger(sessionPreferences)
+    )
+
+    override fun debug(tag: String, message: String) {
+        loggers.forEach { it.debug(tag, message) }
+    }
+
+    override fun info(tag: String, message: String) {
+        loggers.forEach { it.info(tag, message) }
+    }
+
+    override fun error(tag: String, message: String, error: Throwable) {
+        loggers.forEach { it.error(tag, message, error) }
+    }
+}
