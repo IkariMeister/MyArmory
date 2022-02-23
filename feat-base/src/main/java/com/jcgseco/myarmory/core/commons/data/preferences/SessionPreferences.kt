a388b75c66d0244e670f98048de5944afa578dff
Juan Carlos Garcia <jcgseco@gmail.com>
2022-02-23 20:16:05 +0100

    detekt

--- feat-base/src/main/java/com/jcgseco/myarmory/core/commons/data/preferences/SessionPreferences.kt
+++ feat-base/src/main/java/com/jcgseco/myarmory/core/commons/data/preferences/SessionPreferences.kt
@@ -6,7 +6,6 @@ import javax.inject.Inject
 
 open class SessionPreferences @Inject constructor(context: Context) {
 
-
     private val sharedPreferences = context.getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE)
 
     @SuppressLint("ApplySharedPref")
